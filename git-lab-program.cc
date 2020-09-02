947526e156cef34d365a511ed86028a3d2ac4174
Eric Nelson <en062419@ohio.edu>
2020-09-02 17:21:09 -0400

    Added git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Eric Nelson
+ *        Date: 9/2/2020
+ * Description: Test Program, no significant functionality
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+} // main
